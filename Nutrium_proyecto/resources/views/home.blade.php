9947dc480901cd285ead8684b5b7491a79b3cbb2
Sebastian Cardona Rios <secardonar@unal.edu.co>
2023-09-10 21:30:03 -0500

    Primeras historias de usuario

---
+++ Nutrium_proyecto/resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="col">
+                    <button type="button" class="btn btn-success">IPS</button>
+                </div>
+                <br><br><br>
+                <div class="col">
+                    <button type="button" class="btn btn-success">CONSULTAS</button>
+                </div>
+                <br><br><br>
+                <div class="col">
+                    <button type="button" class="btn btn-success">ACCIDENTES DE TRABAJO</button>
+                </div>
+                <br><br><br>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
